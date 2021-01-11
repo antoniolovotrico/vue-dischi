7bd32438733ca8ddee8fa0258d68b952a563536a
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-01-11 20:24:07 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+// Servendoci di Vue JS stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+
+let app = new Vue ({
+    el:"#app",
+    data:{
+        music:[]
+    },
+    methods:{
+        genFunc:function(){
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then(response => {
+                //console.log(response.data.response);
+                this.music.push(response.data.response);
+                console.log(this.music);
+                
+            })
+        }
+    },
+    mounted(){
+        this.genFunc();
+    }
+})
